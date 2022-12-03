e489547ba4be58659b533612ef4130bdd7a955c8
darryl.535210011@stu.untar.ac.id <darryl.535210011@stu.untar.ac.id>
2022-12-03 22:13:37 +0700

    update validator

--- src/api/v1/validators/auth.js
+++ src/api/v1/validators/auth.js
@@ -4,10 +4,10 @@ module.exports = {
   register: {
     body: {
       email: joi.string().email().required(),
-      full_name: joi.string().min(3).max(100).required(),
-      user_name: joi.string().min(5).max(15).required(),
+      full_name: joi.string().min(1).max(100).required(),
+      user_name: joi.string().min(1).max(15).required(),
       phone_no: joi.string().required(),
-      password: joi.string().min(6).max(32).required(),
+      password: joi.string().min(3).max(32).required(),
     },
   },
 
