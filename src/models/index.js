ac350a25aefee492965dc84dea578770e49b1efa
darryl.535210011@stu.untar.ac.id <darryl.535210011@stu.untar.ac.id>
2022-12-03 22:14:42 +0700

    update

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ const Users = require('./user');
 const shipping = require('./shipping');
 
 mongoose.connect(
-'mongodb://Dionathan:Dionathan@ac-w9ggdwl-shard-00-00.7rchlft.mongodb.net:27017,ac-w9ggdwl-shard-00-01.7rchlft.mongodb.net:27017,ac-w9ggdwl-shard-00-02.7rchlft.mongodb.net:27017/?ssl=true&replicaSet=atlas-ng3ied-shard-0&authSource=admin&retryWrites=true&w=majority'
+'mongodb+srv://darryl:darryl@cluster0.pvv1bmd.mongodb.net/?retryWrites=true&w=majority'
 );
 
 const db = mongoose.connection;
